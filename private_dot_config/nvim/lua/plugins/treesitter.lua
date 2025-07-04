6741f2f458c94706cecd7b01e294feb7a952a707
Givemegitpls <veryangrybarmaley@gmail.com>
2025-07-05 00:49:02 +0300

    nvim treesitter config has been improved

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,22 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		config = function()
-			require("ibl").setup({})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				highlight = {
-					enable = true,
-				},
-			})
-		end,
-	},
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"python",
+				"yaml",
+				"markdown",
+				"bash",
+			},
+
+			sync_install = false,
+			auto_install = true,
+
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
