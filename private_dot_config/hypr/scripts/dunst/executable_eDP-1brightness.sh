cc986dc59f6efaff0915620a1517a8006295044f
Givemegitpls <veryangrybarmaley@gmail.com>
2025-08-06 20:31:17 +0300

    display backlight improvements

--- private_dot_config/hypr/scripts/dunst/executable_eDP-1brightness.sh
+++ private_dot_config/hypr/scripts/dunst/executable_eDP-1brightness.sh
@@ -2,9 +2,7 @@
 
 msgId="3378455"
 
-brightnessctl "$@" >/dev/null
-
-brightpercent=$(brightnessctl -m --device='amdgpu_bl1' | awk -F, '{print substr($4, 0, length($4)-1)}')
+brightpercent=$(brightnessctl -m --class='backlight' | awk -F, '{print substr($4, 0, length($4)-1)}')
 
 notify-send -i $HOME/.config/hypr/scripts/dunst/icons/brightness.svg -a "changeBrightness" -u low -r "$msgId" \
   -h int:value:"$brightpercent" "Яркость экрана: $brightpercent%"
--- private_dot_config/hypr/scripts/executable_rog.sh
+++ private_dot_config/hypr/scripts/executable_rog.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-asusctl fan-curve -m "quiet" -f cpu -D 20c:0%,50c:10%,55c:15%,60c:25%,65c:40%,70c:60%,75c:80%,80c:100%
-asusctl fan-curve -m "quiet" -f gpu -D 20c:0%,50c:10%,55c:15%,60c:25%,65c:40%,70c:60%,75c:80%,80c:100%
+asusctl fan-curve -m "quiet" -f cpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
+asusctl fan-curve -m "quiet" -f gpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
 asusctl fan-curve -m "quiet" -e true
-asusctl fan-curve -m "balanced" -f cpu -D 24c:0%,40c:15%,50c:25%,55c:35%,60c:50%,65c:65%,70c:80%,75c:100%
-asusctl fan-curve -m "balanced" -f gpu -D 24c:0%,40c:15%,50c:25%,55c:35%,60c:50%,65c:65%,70c:80%,75c:100%
+asusctl fan-curve -m "balanced" -f cpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
+asusctl fan-curve -m "balanced" -f gpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
 asusctl fan-curve -m "balanced" -e true
 asusctl fan-curve -m "performance" -f cpu -D 24c:10%,35c:25%,40c:40%,50c:60%,55c:75%,60c:85%,65c:95%,70c:100%
 asusctl fan-curve -m "performance" -f gpu -D 24c:10%,35c:25%,40c:40%,50c:60%,55c:75%,60c:85%,65c:95%,70c:100%
