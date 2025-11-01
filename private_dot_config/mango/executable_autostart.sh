c5ab3f615b58745fda5bc0d09b17e66ef7b34b92
Givemegitpls <veryangrybarmaley@gmail.com>
2025-11-01 07:21:15 +0300

    slurp blur has been fixed

--- private_dot_config/mango/executable_autostart.sh
+++ private_dot_config/mango/executable_autostart.sh
@@ -1,8 +1,14 @@
 uwsm finalize
-uwsm app -- ~/.config/mango/scripts/waymonman.py &
-uwsm app -- ~/.config/waybar/toggle.sh -c ~/.config/waybar/config-universal.jsonc &
 uwsm app -- swww-daemon &
 uwsm app -- hypridle &
 uwsm app -- dunst &
+uwsm app -- /usr/lib/xdg-desktop-portal &
+uwsm app -- /usr/lib/xdg-desktop-portal-gtk &
+uwsm app -- ~/.config/mango/scripts/waymonman.py &
 uwsm app -- ~/.config/dunst/scripts/BSC.py &
-uwsm app -- systemd-inhibit --who="Mango config" --why="wlogout keybind" --what=handle-power-key --mode=block sleep infinity
+uwsm app -- ~/.config/waybar/toggle.sh -c ~/.config/waybar/config-universal.jsonc &
+uwsm app -- wl-paste --type text --watch cliphist store &
+uwsm app -- wl-paste --type image --watch cliphist store &
+uwsm app -- systemd-inhibit --who="Mango config" --why="wlogout keybind" --what=handle-power-key --mode=block sleep infinity &
+uwsm app -- noisetorch -i alsa_input.pci-0000_04_00.6.analog-stereo &
+~/.config/hypr/scripts/rog.sh
