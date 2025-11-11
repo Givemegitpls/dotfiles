86e512f243abf8d879d68dedd2bdc93f9a649035
Givemegitpls <veryangrybarmaley@gmail.com>
2025-11-12 02:08:13 +0300

    rofi mango exit menu

---
+++ private_dot_config/mango/scripts/executable_rofi-mango-menu.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+menu_content() {
+  echo -en "\0message\x1fQuit mango?\n"
+  echo "No"
+  echo "Yes"
+}
+
+handle_selection() {
+  case x"$@" in
+  x"No")
+    exit 0
+    ;;
+  x"Yes")
+    mmsg -d quit
+    ;;
+  esac
+}
+
+if [ -n "$ROFI_RETV" ]; then
+  # ROFI_RETV=0 - show menu, ROFI_RETV=1 - selection made
+  if [ "$ROFI_RETV" -eq 0 ]; then
+    menu_content
+  elif [ "$ROFI_RETV" -eq 1 ]; then
+    handle_selection "$1"
+  fi
+else
+  rofi -show quit -modi "quit:$0" -theme-str 'inputbar {enabled: false;} window {height: 200px;}'
+fi
