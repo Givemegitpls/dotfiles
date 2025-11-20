2d247b514c0022580b5626b3b36e921e505566f2
Givemegitpls <veryangrybarmaley@gmail.com>
2025-11-20 15:34:00 +0300

    hyprland purge

--- private_dot_config/mango/executable_autostart.sh
+++ private_dot_config/mango/executable_autostart.sh
@@ -5,9 +5,9 @@ uwsm app -- hypridle &
 uwsm app -- dunst &
 uwsm app -- ~/.config/mango/scripts/waymonman.py &
 uwsm app -- ~/.config/dunst/scripts/BSC.py &
-uwsm app -- ~/.config/waybar/toggle.sh -c ~/.config/waybar/config-universal.jsonc &
+uwsm app -- ~/.config/waybar/toggle.sh &
 uwsm app -- wl-paste --type text --watch cliphist store &
 uwsm app -- wl-paste --type image --watch cliphist store &
 uwsm app -- systemd-inhibit --who="Mango config" --why="wlogout keybind" --what=handle-power-key --mode=block sleep infinity &
 uwsm app -- noisetorch -i alsa_input.pci-0000_04_00.6.analog-stereo &
-~/.config/hypr/scripts/rog.sh
+~/.config/mango/scripts/rog.sh
