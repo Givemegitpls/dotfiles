109d86890994c38dd9bd8c7e8b45e6aa52ba6ea3
Givemegitpls <veryangrybarmaley@gmail.com>
2025-12-01 23:27:07 +0300

    nvim visual improvements faster cursor

--- private_dot_config/nvim/lua/plugins/visual.lua
+++ private_dot_config/nvim/lua/plugins/visual.lua
@@ -8,6 +8,9 @@ return {
 			scroll_buffer_space = true,
 			legacy_computing_symbols_support = false,
 			smear_insert_mode = true,
+			stiffness = 0.8,
+			trailing_stiffness = 0.5,
+			distance_stop_animating = 0.5,
 		},
 	},
 	-- main theme
