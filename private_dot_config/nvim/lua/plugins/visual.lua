c80f8195cac628bd864fe657956049c7ac9f7b09
Givemegitpls <veryangrybarmaley@gmail.com>
2025-12-01 23:21:40 +0300

    nvim visual improvements

---
+++ private_dot_config/nvim/lua/plugins/visual.lua
@@ -0,0 +1,69 @@
+return {
+	-- cursor theme
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			smear_between_buffers = true,
+			smear_between_neighbor_lines = true,
+			scroll_buffer_space = true,
+			legacy_computing_symbols_support = false,
+			smear_insert_mode = true,
+		},
+	},
+	-- main theme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "auto", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = true,
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	-- statusbar
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	-- git helper
+	{
+		"akinsho/git-conflict.nvim",
+		config = function()
+			local git = require("git-conflict")
+			git.setup({
+				default_mappings = true, -- disable buffer local mapping created by this plugin
+				default_commands = true, -- disable commands created by this plugin
+				disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+				list_opener = "copen", -- command or function to open the conflicts list
+				highlights = { -- They must have background color, otherwise the default color will be used
+					incoming = "DiffAdd",
+					current = "DiffText",
+				},
+			})
+		end,
+	},
+	-- tree branch visualizer
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("ibl").setup()
+		end,
+	},
+	-- markdown renderer
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+		opts = {},
+	},
+}
