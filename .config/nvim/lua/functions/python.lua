233f800d16c96d07112738487e54ec1f1fe60736
Givemegitpls <veryangrybarmaley@gmail.com>
2026-01-12 16:34:52 +0300

    nvim functions/python.lua has been updated

--- .config/nvim/lua/functions/python.lua
+++ .config/nvim/lua/functions/python.lua
@@ -1,59 +1,56 @@
 -- source https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-877293306
 local export = {}
 
-local function shorten_path(path)
-	if not path or #path <= 1 then
-		return path or "/"
+local function shorten_path(workspace)
+	if not workspace or #workspace <= 1 then
+		return workspace or "/"
 	end
 
-	local last_slash = path:match(".*/()")
+	local last_slash = workspace:match(".*/()")
 
 	if last_slash and last_slash > 1 then
-		return path:sub(1, last_slash - 2)
+		return workspace:sub(1, last_slash - 2)
 	else
 		return "/"
 	end
 end
 
-local function check_poetry(path, workspace)
-	local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+local function check_poetry(workspace)
+	local match = vim.fn.glob(vim.fs.joinpath(workspace, "poetry.lock"))
 	if match ~= "" then
 		vim.api.nvim_set_current_dir(workspace)
 		local venv = vim.fn.trim(vim.fn.system("poetry  env info -p 2> /dev/null"))
 		if venv ~= "" then
-			return path.join(venv, "bin", "python")
+			return vim.fs.joinpath(venv, "bin", "python")
 		end
 	end
 	return ""
 end
 
-local function check_venv(path, workspace)
-	local match = vim.fn.glob(path.join(workspace, ".venv"))
+local function check_venv(workspace)
+	local match = vim.fn.glob(vim.fs.joinpath(workspace, ".venv"))
 	if match ~= "" then
 		vim.api.nvim_set_current_dir(workspace)
-		return path.join(match, "bin", "python")
+		return vim.fs.joinpath(match, "bin", "python")
 	end
 	return ""
 end
 
 function export.get_python_path()
-	local util = require("lspconfig/util")
-	local path = util.path
-
 	workspace = vim.fn.expand("%:p")
 	-- Use activated virtualenv.
 	if vim.env.VIRTUAL_ENV then
-		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+		return vim.fs.joinpath(vim.env.VIRTUAL_ENV, "bin", "python")
 	end
 
 	-- Find and use virtualenv via poetry in workspace directory.
 	if workspace ~= "" then
 		while workspace ~= "" do
-			local poetry = check_poetry(path, workspace)
+			local poetry = check_poetry(workspace)
 			if poetry ~= "" then
 				return poetry
 			end
-			local venv = check_venv(path, workspace)
+			local venv = check_venv(workspace)
 			if venv ~= "" then
 				return venv
 			end
