a47fd159037bd6e523874da788e7a8af2d92fb01
Givemegitpls <veryangrybarmaley@gmail.com>
2026-01-26 22:34:42 +0300

    asus stuf fix

--- .scripts/ROGstartup.sh
+++ .scripts/ROGstartup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-asusctl fan-curve -m "quiet" -f cpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
-asusctl fan-curve -m "quiet" -f gpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
-asusctl fan-curve -m "quiet" -e true
-asusctl fan-curve -m "balanced" -f cpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
-asusctl fan-curve -m "balanced" -f gpu -D 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
-asusctl fan-curve -m "balanced" -e true
-asusctl fan-curve -m "performance" -f cpu -D 24c:10%,35c:25%,40c:40%,50c:60%,55c:75%,60c:85%,65c:95%,70c:100%
-asusctl fan-curve -m "performance" -f gpu -D 24c:10%,35c:25%,40c:40%,50c:60%,55c:75%,60c:85%,65c:95%,70c:100%
-asusctl fan-curve -m "performance" -e true
-asusctl profile -P "balanced"
+asusctl fan-curve --mod-profile Quiet --fan cpu --data 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
+asusctl fan-curve --mod-profile Quiet --fan gpu --data 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
+asusctl fan-curve --mod-profile Quiet --enable-fan-curves true
+asusctl fan-curve --mod-profile Balanced --fan cpu --data 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
+asusctl fan-curve --mod-profile Balanced --fan gpu --data 0c:0%,60c:5%,65c:15%,70c:25%,75c:35%,80c:45%,85c:70%,90c:100%
+asusctl fan-curve --mod-profile Balanced --enable-fan-curves true
+asusctl fan-curve --mod-profile Performance --fan cpu --data 24c:10%,35c:25%,40c:40%,50c:60%,55c:75%,60c:85%,65c:95%,70c:100%
+asusctl fan-curve --mod-profile Performance --fan gpu --data 24c:10%,35c:25%,40c:40%,50c:60%,55c:75%,60c:85%,65c:95%,70c:100%
+asusctl fan-curve --mod-profile Performance --enable-fan-curves true
+sleep 3
+asusctl profile set Balanced
