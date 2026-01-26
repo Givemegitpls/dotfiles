75bfe03ebfd798685981278527d5713053a94bea
Givemegitpls <veryangrybarmaley@gmail.com>
2026-01-26 20:15:37 +0300

    some attempts to refactor this...

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -3,11 +3,10 @@ uwsm app -- dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CUR
 uwsm app -- swww-daemon &
 uwsm app -- hypridle &
 uwsm app -- dunst &
-uwsm app -- ~/.config/mango/scripts/waymonman.py &
-uwsm app -- ~/.config/mango/scripts/ddcutil.py &
+uwsm app -- waymonman &
 uwsm app -- ~/.config/waybar/toggle.sh &
 uwsm app -- wl-paste --type text --watch cliphist store &
 uwsm app -- wl-paste --type image --watch cliphist store &
 uwsm app -- systemd-inhibit --who="Mango config" --why="wlogout keybind" --what=handle-power-key --mode=block sleep infinity &
 uwsm app -- noisetorch -i &
-~/.config/mango/scripts/ROGstartup.sh
+~/.scripts/ROGstartup.sh
