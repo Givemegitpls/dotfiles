1f6507ddabeab8bba9a74fc7c4325ca25225e756
Givemegitpls <veryangrybarmaley@gmail.com>
2026-02-25 21:51:10 +0300

    yazi theme autoinstall

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,3 +1,10 @@
+-- install theme
+if not os.execute("ls $HOME/.config/yazi/theme.toml") then
+	os.execute(
+		"curl https://raw.githubusercontent.com/catppuccin/yazi/refs/heads/main/themes/mocha/catppuccin-mocha-blue.toml | sed '/\\[app\\]/,+1d' > $HOME/.config/yazi/theme.toml"
+	)
+end
+
 -- install plugins
 local packages = {
 	"yazi-rs/plugins:mount",
@@ -14,10 +21,10 @@ end
 do
 	local command = "ya pkg list"
 	local handle = io.popen(command, "r")
-	local report = handle:read("*a")
-	local return_status = handle:close()
-
-	for _, package in ipairs(packages) do
-		install_if_not_exists(package, report)
+	if handle ~= nil then
+		local report = handle:read("*a")
+		for _, package in ipairs(packages) do
+			install_if_not_exists(package, report)
+		end
 	end
 end
