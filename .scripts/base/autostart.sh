618554dc218018e3e816277f645ef94833934add
Givemegitpls <veryangrybarmaley@gmail.com>
2026-02-25 20:19:48 +0300

    autostart script path has been changed

--- .scripts/base/autostart.sh
+++ .scripts/base/autostart.sh
@@ -1,5 +1,4 @@
 uwsm finalize
-uwsm app -- dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
 uwsm app -- swww-daemon &
 uwsm app -- hypridle &
 uwsm app -- dunst &
@@ -7,6 +6,6 @@ uwsm app -- waymonman &
 uwsm app -- ~/.config/waybar/scripts/waybar_toggle.sh &
 uwsm app -- wl-paste --type text --watch cliphist store &
 uwsm app -- wl-paste --type image --watch cliphist store &
-uwsm app -- systemd-inhibit --who="Mango config" --why="wlogout keybind" --what=handle-power-key --mode=block sleep infinity &
+uwsm app -- systemd-inhibit --who="$XDG_CURRENT_DESKTOP config" --why="wlogout keybind" --what=handle-power-key --mode=block sleep infinity &
 uwsm app -- noisetorch -i &
 ~/.scripts/asus/ROGstartup.sh
