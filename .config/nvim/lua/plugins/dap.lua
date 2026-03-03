f503bc87a030736d66b437a86da2208773e752e6
Givemegitpls <veryangrybarmaley@gmail.com>
2026-03-03 18:57:35 +0300

    nvim dapui update

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -5,6 +5,38 @@ return {
 	{
 		"rcarriga/nvim-dap-ui",
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+			dapui.setup({ ---@diagnostic disable-line: missing-fields
+				layouts = {
+					{
+						elements = {
+							{ id = "console", size = 1 },
+						},
+						position = "bottom",
+						size = 10,
+					},
+				},
+				floating = {
+					border = "rounded",
+					mappings = {
+						["close"] = { "q", "<Esc>" },
+					},
+				},
+			})
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
 	},
 	{
 		"mfussenegger/nvim-dap-python",
