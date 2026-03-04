02f18dabaf8706ccd4e99b744f4cc58e85a857f7
Givemegitpls <veryangrybarmaley@gmail.com>
2026-03-04 18:10:42 +0300

    nvim trail has been removed (hello kitty)

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,18 +1,4 @@
 return {
-	-- cursor theme
-	{
-		"sphamba/smear-cursor.nvim",
-		opts = {
-			smear_between_buffers = true,
-			smear_between_neighbor_lines = true,
-			scroll_buffer_space = true,
-			legacy_computing_symbols_support = false,
-			smear_insert_mode = true,
-			stiffness = 0.8,
-			trailing_stiffness = 0.5,
-			distance_stop_animating = 0.5,
-		},
-	},
 	-- main theme
 	{
 		"catppuccin/nvim",
