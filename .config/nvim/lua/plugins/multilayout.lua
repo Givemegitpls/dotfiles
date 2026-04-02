3d036909baa4ede27618880d09a8815cc32e2798
Givemegitpls <veryangrybarmaley@gmail.com>
2026-04-02 15:37:43 +0300

    nvim multilang layout

---
+++ .config/nvim/lua/plugins/multilayout.lua
@@ -0,0 +1,22 @@
+return {
+	-- Minimal configuration for russian layout using lazy.nvim
+	{
+		"mrsobakin/multilayout.nvim",
+		opts = {
+			layouts = {
+				ru = "ru",
+			},
+			-- Enable if you want to have full multilayout.nvim functionality.
+			use_libukb = false,
+		},
+	},
+	{
+		"Wansmer/langmapper.nvim",
+		lazy = false,
+		priority = 1, -- High priority is needed if you will use `autoremap()`
+		config = function()
+			require("langmapper").setup({--[[ your config ]]
+			})
+		end,
+	},
+}
