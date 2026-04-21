2fc529f3e248825910f9081541b0ad685cda3c2f
Givemegitpls <veryangrybarmaley@gmail.com>
2026-04-21 13:49:08 +0300

    volume notification has been improved

--- .scripts/media/volume-changer.sh
+++ .scripts/media/volume-changer.sh
@@ -2,18 +2,29 @@
 
 msgId="3378455"
 
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/Volume://' |
-  sed 's/ 0.//' | tr -d ' .')
+icons_path=$HOME/.local/share/icons
 
 case "$1" in
 "up")
   wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1.0
-  notify-send -i $HOME/.local/share/icons/volume-up.svg -a "changevolume" -u low -r "$msgId" \
-    -h int:value:"$volume" "Громкость: $volume%"
   ;;
 "down")
   wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- -l 0.0
-  notify-send -i $HOME/.local/share/icons/volume-down.svg -a "changevolume" -u low -r "$msgId" \
-    -h int:value:"$volume" "Громкость: $volume%"
   ;;
 esac
+
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/Volume://' |
+  sed 's/ 0.//' | tr -d ' .')
+
+if (($volume > 75)); then
+  icon=$icons_path/volume-high.svg
+elif (($volume > 35)); then
+  icon=$icons_path/volume-medium.svg
+elif (($volume > 0)); then
+  icon=$icons_path/volume-low.svg
+else
+  icon=$icons_path/volume-mute.svg
+fi
+
+notify-send -i $icon -a "changevolume" -u low -r "$msgId" \
+  -h int:value:"$volume" "Громкость: $volume%"
--- .scripts/media/volume-mute.sh
+++ .scripts/media/volume-mute.sh
@@ -4,10 +4,24 @@ wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
 
 msgId="3378455"
 
+icons_path=$HOME/.local/share/icons
+
 station=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/Volume://' | tr -d [:digit:] | tr -d ' .[]')
 
-if [ $station = 'MUTED' ]; then
-  notify-send -i $HOME/.local/share/icons/volume-mute.svg -u low -r "$msgId" "Звук выключен"
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/Volume://' | sed 's/ 0.//' | tr -d ' .')
+
+if [ "$station" = "MUTED" ] || [ $volume -eq 0 ]; then
+  icon=$icons_path/volume-mute.svg
+  msg="Звук выключен"
 else
-  notify-send -i $HOME/.local/share/icons/volume-up.svg -u low -r "$msgId" "Звук включен"
+  msg="Звук включен"
+  if (($volume > 75)); then
+    icon=$icons_path/volume-high.svg
+  elif (($volume > 35)); then
+    icon=$icons_path/volume-medium.svg
+  else
+    icon=$icons_path/volume-low.svg
+  fi
 fi
+
+notify-send -i $icon -u low -r "$msgId" "$msg"
