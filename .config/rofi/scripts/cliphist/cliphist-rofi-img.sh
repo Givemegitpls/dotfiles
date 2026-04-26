6ba40645387ed628e0412004f9eeb5a33ca28c96
Givemegitpls <veryangrybarmaley@gmail.com>
2026-04-26 21:43:43 +0300

    rofi clipboard update

--- .config/rofi/scripts/cliphist/cliphist-rofi-img.sh
+++ .config/rofi/scripts/cliphist/cliphist-rofi-img.sh
@@ -3,8 +3,8 @@
 tmp_dir="/tmp/cliphist"
 rm -rf "$tmp_dir"
 
-if [[ -n "$1" ]]; then
-  cliphist decode <<<"$1" | wl-copy
+if [[ -n "$ROFI_INFO" ]]; then
+  cliphist decode "$ROFI_INFO" | wl-copy
   exit
 fi
 
@@ -14,9 +14,19 @@ read -r -d '' prog <<EOF
 /^[0-9]+\s<meta http-equiv=/ { next }
 match(\$0, /^([0-9]+)\s(\[\[\s)?binary.*(jpg|jpeg|png|bmp)/, grp) {
     system("echo " grp[1] "\\\\\t | cliphist decode >$tmp_dir/"grp[1]"."grp[3])
-    print \$0"\0icon\x1f$tmp_dir/"grp[1]"."grp[3]
+    
+    line = \$0
+    sub(/^[0-9]+\t/, "", line)
+    
+    print line "\0info\x1f" grp[1] "\x1ficon\x1f$tmp_dir/"grp[1]"."grp[3]
     next
 }
-1
+{
+    id = \$0
+    sub(/\t.*/, "", id)
+    line = \$0
+    sub(/^[0-9]+\t/, "", line)
+    print line "\0info\x1f" id
+}
 EOF
 cliphist list | gawk "$prog"
