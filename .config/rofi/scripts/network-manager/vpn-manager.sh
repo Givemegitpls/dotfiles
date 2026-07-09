4968194112a65519368335f7f9c263472dc2af39
Givemegitpls <veryangrybarmaley@gmail.com>
2026-07-09 18:32:09 +0300

    Revert "mihomo has been removed"
    This reverts commit 7de9bb0f8983e8074cc1585a580e88a38f3f58b8.

--- .config/rofi/scripts/network-manager/vpn-manager.sh
+++ .config/rofi/scripts/network-manager/vpn-manager.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Проверяем sing-box и netbird
 case x"$@" in
+x"  Stop mihomo")
+  coproc (systemctl --user stop mihomo >/dev/null 2>&1) &
+  exit 0
+  ;;
+x"  Start mihomo")
+  coproc (systemctl --user start mihomo >/dev/null 2>&1) &
+  exit 0
+  ;;
 x"  Stop sing-box")
   coproc (systemctl --user stop sing-box >/dev/null 2>&1) &
   exit 0
@@ -21,9 +29,17 @@ esac
 
 active="\0active\x1f"
 
+# Добавляем mihomo
+if systemctl --user is-active --quiet mihomo; then
+  active+="0,"
+  echo "  Stop mihomo"
+else
+  echo "  Start mihomo"
+fi
+
 # Добавляем sing-box
 if systemctl --user is-active --quiet sing-box; then
-  active+="0,"
+  active+="1,"
   echo "  Stop sing-box"
 else
   echo "  Start sing-box"
@@ -38,7 +54,7 @@ fi
 if [ -z "$netbird_status" ]; then
   echo "  Start netbird"
 else
-  active+="1"
+  active+="2"
   echo "  Stop netbird"
 fi
 echo -en "$active\n"
